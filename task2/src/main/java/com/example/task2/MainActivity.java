128603b1c8baa0349f5784ceb944f55ef5a9915d
Max <mks59k@gmail.com>
2019-08-22 16:46:58 +0300

    Реализован тестовый вывод координат перемещения

--- task2/src/main/java/com/example/task2/MainActivity.java
+++ task2/src/main/java/com/example/task2/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.task2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvX;
+    private TextView tvY;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvX = this.findViewById(R.id.tvX);
+        this.tvY = this.findViewById(R.id.tvY);
+
+        LinearLayout LL = this.findViewById(R.id.ll1);
+
+        LL.setOnTouchListener(
+                new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                        int action = motionEvent.getAction();
+
+                        float x = motionEvent.getX();
+                        float y = motionEvent.getY();
+
+                        String textX = "X = ";
+                        String textY = "Y = ";
+                        String text = "";
+//
+                        if (action == MotionEvent.ACTION_MOVE){
+                            text += "Перемещение: (" + action + ")" + "\n";
+                        }
+
+
+                        text += "X = " + x + "\nY = " + y;
+
+                        MainActivity.this.tvX.setText(text);
+
+                        return false;
+                    }
+                }
+        );
+
+        LL.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
     }
 }
