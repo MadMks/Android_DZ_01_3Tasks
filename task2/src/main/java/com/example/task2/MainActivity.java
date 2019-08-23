a56414c0de1b75b9d3111751dd8dc80dc93de810
Max <mks59k@gmail.com>
2019-08-23 14:17:22 +0300

    Реализовано (без мерцаний) определение вертикального и горизонтального движения пальцем

--- task2/src/main/java/com/example/task2/MainActivity.java
+++ task2/src/main/java/com/example/task2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.task2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -16,8 +17,20 @@ public class MainActivity extends AppCompatActivity {
     private TextView tvX;
     private TextView tvY;
 
-    private float x = 0;
-    private float y = 0;
+    private LinearLayout LL;
+
+
+    float x;
+    float y;
+    String sDown;
+    String sMove;
+    String sUp;
+
+    private float oldX;
+    private float oldY;
+
+    private int cntX;
+    private int cntY;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -29,62 +42,66 @@ public class MainActivity extends AppCompatActivity {
         this.tvX = this.findViewById(R.id.tvX);
         this.tvY = this.findViewById(R.id.tvY);
 
-        LinearLayout LL = this.findViewById(R.id.ll1);
+        this.LL = this.findViewById(R.id.ll1);
+
 
         LL.setOnTouchListener(
                 new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View view, MotionEvent motionEvent) {
 
-                        int action = motionEvent.getAction();
+                        x = motionEvent.getX();
+                        y = motionEvent.getY();
 
-                        float tempX = motionEvent.getX();
-                        float tempY = motionEvent.getY();
+                        tvX.setText("_");
+                        tvY.setText("_");
 
-                        String textX = "X = ";
-                        String textY = "Y = ";
-                        String textTV = "_";
-                        String text = "";
-//
-                        if (action == MotionEvent.ACTION_MOVE){
+                        int margin = 50;
 
-                            
-                            if (
-                                    (tempX != x) && (tempY != y)
+                        switch (motionEvent.getAction()) {
+                            case MotionEvent.ACTION_DOWN: // нажатие
+                                sDown = "Down: " + x + "," + y;
+                                sMove = ""; sUp = "";
+                                oldX = motionEvent.getX();
+                                oldY = motionEvent.getY();
+                                break;
+                            case MotionEvent.ACTION_MOVE: // движение
+                                sMove = "Move: " + x + "," + y;
+
+                                if (
+                                    (x > oldX + 20 || x < oldX - 20)
                                     && (
-                                            (tempY < y - 4) || (tempY > y + 4)
-                                            &&
-                                            (tempX < x - 4) || (tempX > x + 4)
-                                            )
-                            ){
-                                textTV += "Перемещение по диагонали";
-                            }
-                            else if (
-                                    (tempX != x)
-                                    && ((tempY > y - 3) && (tempY < y + 3))
-                            ) {
-                                textTV += "Перемещение по X: ";
-                            }
-                            else if (
-                                    (tempX != y)
-                                            && ((tempX > x - 3) && (tempX < x + 3))
-                            ) {
-                                textTV += "Перемещение по Y: ";
-                            }
+                                        y > (oldY - margin)
+                                        &&
+                                        y < (oldY + margin)
+                                    )
+                                ){
+//                                    oldX = motionEvent.getX();
+                                    tvX.setText("Движение по X" + "___ cntX=" + cntX);
+                                }
 
+                                if (
+                                    (y > oldY + 20 || y < oldY - 20)
+                                    && (
+                                        x > (oldX - margin)
+                                        &&
+                                        x < (oldX + margin)
+                                        )
+                                ) {
+//                                    oldY = motionEvent.getY();
+                                    tvY.setText("Движение по Y");
+                                }
 
-                            x = tempX;
-                            y = tempY;
+                                break;
+                            case MotionEvent.ACTION_UP: // отпускание
+                            case MotionEvent.ACTION_CANCEL:
+                                sMove = "";
+                                sUp = "Up: " + x + "," + y;
+                                break;
                         }
+                        tv.setText(sDown + "\n" + sMove + "\n" + sUp);
 
-
-                        text += "tempX = " + tempX + "\ntempY = " + tempY;
-
-                        MainActivity.this.tv.setText(textTV);
-                        MainActivity.this.tvX.setText(text);
-                        MainActivity.this.tvY.setText("x = " + x + "\ny = " + y);
-
-                        return false;
+                        return true;
                     }
                 }
         );
