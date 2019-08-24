4056cc2dea52b0265d568ff6ae7c90b31991d8df
Max <mks59k@gmail.com>
2019-08-24 14:37:11 +0300

    Реализован тестовый сброс табло, вывод цифр и запись в очередь цыфр и знаков.

--- task3/src/main/java/com/example/task3/Calculator.java
+++ task3/src/main/java/com/example/task3/Calculator.java
@@ -1,14 +1,29 @@
 package com.example.task3;
 
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.Deque;
 
 public class Calculator {
 
-    private int[] numberArr;
-    private String[] signArr;
+    private final static String TAG = "=== Calculator ";
+
+    private Deque numbers;
+    private Deque<String> signs;
     private TextView textView;
 
     public Calculator(TextView textViewForScreen) {
         this.textView = textViewForScreen;
+    }
+
+    public void setNumber(int digit) {
+        numbers.addLast(digit);
+        Log.d(TAG, "added number: " + numbers.peekLast());
+    }
+
+    public void setSign(String sign) {
+        signs.addLast(sign);
+        Log.d(TAG, "added sign: " + signs.peekLast());
     }
 }
--- task3/src/main/java/com/example/task3/MainActivity.java
+++ task3/src/main/java/com/example/task3/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.task3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "=== MainActivity ";
+
     private Calculator calculator;
     private TextView tvScreen;
 
     // Кнопки
+    private Button btnC;
+
     private Button btn1;
     private Button btn2;
 
@@ -29,23 +34,60 @@ public class MainActivity extends AppCompatActivity {
         this.calculator = new Calculator(tvScreen);
 
         // Присваиваем кнопки по id
+        this.btnC = this.findViewById(R.id.btn_C);
+
         this.btn1 = this.findViewById(R.id.btn_1);
         this.btn2 = this.findViewById(R.id.btn_2);
 
+        // Значение экрана по умолчанию
+        tvScreen.setText("0");
+
+        // Один обработчик для всех нажатий
+        // TODO: переделать на несколько обработчиков
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Button clickBtn = findViewById(view.getId());
 
+                // Log
+                Log.d(TAG, "onClick " + clickBtn.getText());
+
                 switch (view.getId()){
+                    case R.id.btn_C:
+                        // Вывести ноль на экран
+                        tvScreen.setText("0");
+                        // TODO обнулить массивы
+                        break;
                     case R.id.btn_1:
                     case R.id.btn_2:
-                        tvScreen.setText("Нажата цифра" + clickBtn.getText());
+                        // TODO отобразить на экране
+                        String str = "0";
+                        Log.d(TAG, tvScreen.getText().toString());
+                        if (tvScreen.getText() != "0"){
+                            str = tvScreen.getText().toString() +
+                                    clickBtn.getText().toString();
+                        }
+                        else{
+                            str = clickBtn.getText().toString();
+                        }
+                        tvScreen.setText(str);
+                        break;
+                        // + - * /
+                    case R.id.btn_plus:
+                        // TODO записать число и знак
+                        int num = Integer.getInteger(tvScreen.getText().toString());
+                        String sign = clickBtn.getText().toString();
+
+                        calculator.setNumber(num);
+                        calculator.setSign(sign);
                         break;
                 }
             }
         };
+
+
+        btnC.setOnClickListener(onClickListener);
 
         btn1.setOnClickListener(onClickListener);
         btn2.setOnClickListener(onClickListener);
